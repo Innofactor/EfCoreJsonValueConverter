fe738b56476cf148cab4e65d74507033495cfabe
jukka.hyvarinen <jukka.hyvarinen@innofactor.com>
2018-06-14 14:24:53 +0300

    adding comments

--- Innofactor.EfCoreJsonValueConverter.Attributes/JsonFieldAttribute.cs
+++ Innofactor.EfCoreJsonValueConverter.Attributes/JsonFieldAttribute.cs
@@ -2,6 +2,9 @@
 
 namespace Innofactor.EfCoreJsonValueConverter {
 
+  /// <summary>
+  /// Marks the property to be serialized into database as JSON.
+  /// </summary>
   [AttributeUsage(AttributeTargets.Property, Inherited = false, AllowMultiple = false)]
   public class JsonFieldAttribute : Attribute {}
 
