246b40d0c3239087fceafd07506d1898966c24ac
jukka.hyvarinen <jukka.hyvarinen@innofactor.com>
2018-07-23 12:39:20 +0300

    some refactor

--- Innofactor.EfCoreJsonValueConverter/ModelBuilderExtensions.cs
+++ Innofactor.EfCoreJsonValueConverter/ModelBuilderExtensions.cs
@@ -3,6 +3,7 @@ using Microsoft.EntityFrameworkCore.ChangeTracking;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using System;
 using System.Linq;
+using System.Reflection;
 
 namespace Innofactor.EfCoreJsonValueConverter {
 
@@ -10,6 +11,11 @@ namespace Innofactor.EfCoreJsonValueConverter {
   /// Extensions for <see cref="ModelBuilder"/>.
   /// </summary>
   public static partial class ModelBuilderExtensions {
+
+    private static bool HasJsonAttribute(PropertyInfo propertyInfo) {
+      var attributes = propertyInfo?.GetCustomAttributes(typeof(JsonFieldAttribute), false);
+      return attributes != null && attributes.Any();
+    }
 
     /// <summary>
     /// Add fields marked with <see cref="JsonFieldAttribute"/> to be converted using <see cref="JsonValueConverter{T}"/>.
@@ -24,17 +30,16 @@ namespace Innofactor.EfCoreJsonValueConverter {
         throw new ArgumentNullException(nameof(modelBuilder));
 
       foreach (var entityType in modelBuilder.Model.GetEntityTypes()) {
-        foreach (var property in entityType.GetProperties().Where(p => p?.PropertyInfo != null)) {
-          var attributes = property.PropertyInfo?.GetCustomAttributes(typeof(JsonFieldAttribute), false);
-          if (attributes.Any()) {
-            var modelType = property.PropertyInfo.PropertyType;
-            var converterType = typeof(JsonValueConverter<>).MakeGenericType(modelType);
-            var converter = (ValueConverter)Activator.CreateInstance(converterType, new object[] { null });
-            property.SetValueConverter(converter);
-            var valueComparer = typeof(JsonValueComparer<>).MakeGenericType(modelType);
-            property.SetValueComparer((ValueComparer)Activator.CreateInstance(valueComparer, new object[0]));
-          }
+
+        foreach (var property in entityType.GetProperties().Where(p => HasJsonAttribute(p.PropertyInfo))) {
+          var modelType = property.PropertyInfo.PropertyType;
+          var converterType = typeof(JsonValueConverter<>).MakeGenericType(modelType);
+          var converter = (ValueConverter)Activator.CreateInstance(converterType, new object[] { null });
+          property.SetValueConverter(converter);
+          var valueComparer = typeof(JsonValueComparer<>).MakeGenericType(modelType);
+          property.SetValueComparer((ValueComparer)Activator.CreateInstance(valueComparer, new object[0]));
         }
+
       }
 
     }
